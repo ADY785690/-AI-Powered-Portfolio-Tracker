aaee0df437da37095270bd24eacd2cd9e09307d3
ADY785690 <adityakrsharma6969@gmail.com>
2026-06-17 16:29:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ df = pd.read_csv("data/portfolio.csv")
 st.subheader("Portfolio Holdings")
 st.dataframe(df)
 
+st.subheader("Portfolio Summary")
+
+total_investment = (df["Quantity"] * df["BuyPrice"]).sum()
+
+st.metric("Total Investment", f"₹{total_investment:,.0f}")
+
+st.bar_chart(df.set_index("Symbol")["Quantity"])
+
 st.subheader("Add New Stock")
 
 symbol = st.text_input("Stock Symbol")
@@ -16,11 +24,3 @@ price = st.number_input("Buy Price", min_value=0.0)
 
 if st.button("Add Stock"):
     st.success(f"{symbol} added successfully!")
-
-st.subheader("Portfolio Summary")
-
-total_investment = (df["Quantity"] * df["BuyPrice"]).sum()
-
-st.metric("Total Investment", f"₹{total_investment:,.2f}")
-
-st.bar_chart(df["Quantity"])
